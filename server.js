d5522717dabb49f7f06a4d360b305d6e2e608261
Enes Demir <enesdemir12@outlook.com.tr>
2019-05-23 22:58:17 +0300

    updated

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ var exportDemo = require("./exportDemo");
 http.createServer(function(request,response){  
     console.log(request.url);
     if(request.url == "/"){
-        response.write("Ana Sayfa ");
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write("<html><body><div><img src='img/enes-demir.png'></div></body></html>");
+    }
+    else if(request.url == "/customer"){
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write("<html><body><div><img src='img/enes-demir.png'></div></body></html>");
     }
     else if(request.url == "/admin"){
         response.write("Admin Sayfasi ");
     }
-    response.end("enes demir");
+    response.end("");
 }).listen(8080);
 
 log.information("Sunucu yayına geçti");
